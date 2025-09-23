a38d9d2a8006f8d8a3054ec7d1335202124bfd73
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2025-09-23 11:40:41 +0200

    add example of map()

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header"
 import MyFirstComponent from "./components/MyFirstComponent"
 import Footer from "./components/Footer"
 import User from "./components/User"
+import MovieList from "./components/MovieList"
 
 
 
@@ -11,10 +12,13 @@ function App() {
       <h1>React Intro</h1>
 
       <Header title='hello world' />
+      
 
       <User userName="alice" age={30} email="alice@example.com" />
       <User userName="bob" age={40} email="bob@example.com" />
       <User userName="charlie" age={50} email="charlie@example.com" />
+
+      <MovieList />
 
       <MyFirstComponent />
 
---
+++ src/components/MovieList.jsx
@@ -0,0 +1,22 @@
+import moviesArr from '../data/movies.json'
+
+
+function MovieList() {
+    return (
+        <section id="movie-list">
+            <h3>List of movies:</h3>
+
+            {moviesArr.map((movieObj, i, arr) => {
+                return (
+                    <div className='box'>
+                        <p>{movieObj.title}</p>
+                        <p>{movieObj.year}</p>
+                        <p>{movieObj.rating}</p>
+                    </div>
+                )
+            })}
+        </section>
+    )
+}
+
+export default MovieList
